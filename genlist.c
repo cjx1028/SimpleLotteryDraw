951d27d78ad46f6708f20510a9d98d3e3a663c08
cjx1028 <cjx1028@gmail.com>
2016-05-11 17:41:14 +0800

    添加一些注释

--- genlist.c
+++ genlist.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void delspace(char * p) {
+void delspace(char * p) { //actually, this function also delete the \n ,^^
 	int i,j=0;
 	for ( i = 0; p[i]!='\0'; i ++ ) {
 		if(p[i] != ' ' && p[i] != '\n')
