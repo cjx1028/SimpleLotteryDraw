dbe0c069bd18b771b32d3b1a9f4e1cb4a84643c2
cjx1028 <cjx1028@gmail.com>
2016-05-11 14:56:37 +0800

    lottery draw game, good luck

---
+++ genlist.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+void delspace(char * p) {
+	int i,j=0;
+	for ( i = 0; p[i]!='\0'; i ++ ) {
+		if(p[i] != ' ' && p[i] != '\n')
+			p[j++] = p[i];
+	}
+	p[j] = '\0';
+}
+
+int main() {
+	FILE * fa, * fb;
+	char strtmp[100];
+	int len = 0;
+
+	fa = fopen("list.txt", "r");
+	fb = fopen("list.js", "w");
+	
+	fprintf(fb, "var ary = new Array(\n");
+
+	while(fgets(strtmp, 100, fa) != NULL) {
+		delspace(strtmp);
+		len = strlen(strtmp);
+
+		if (len !=0){
+			fprintf(fb, "\"%s\",\n", strtmp);
+		}
+
+	}
+	
+	fprintf(fb, "\"todel\"\n");
+	fprintf(fb, ")\n");
+	fprintf(fb, "ary.splice(ary.length-1, 1)\n");
+
+
+	fclose(fa);
+	fclose(fb);
+
+	return 0;
+}
+
+
+
+
+
+
---
+++ run.bat
@@ -0,0 +1,4 @@
+genlist.exe
+
+rem start iexplore %CD%/³é½±.html
+start chrome %CD%/³é½±.html
